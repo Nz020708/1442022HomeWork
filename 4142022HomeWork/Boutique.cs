9afee622fd5c68a90803a92e9399b487475e3e45
NazMzadeh <100687473+Nz020708@users.noreply.github.com>
2022-04-14 11:26:16 +0400

    Add files via upload

---
+++ 4142022HomeWork/Boutique.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _4142022HomeWork
+{
+   public  class Boutique
+    {
+        public int Id { get; set; }
+        public List<Sales> Sales { get; set; }
+        public Sales dress { get; set; }
+        public double TotalSale { get; set; }
+    }
+}
